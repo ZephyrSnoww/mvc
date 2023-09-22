9e4add1dd8332f926e1d41070af04be84ddd1205
Zephyr <zephyr.snow@pm.me>
2023-09-22 07:27:24 -0600

    Incredibly basic functionality

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,25 @@
 <script setup>
+import { reactive } from 'vue';
 
+let state = reactive({
+  modsList: [],
+  search: ''
+});
+
+let localModsList = localStorage.getItem('modsList');
+
+if (localModsList) {
+  state.modsList = JSON.parse(localModsList);
+}
+
+function search() {
+  alert(state.search);
+}
 </script>
 
 <template>
-
+  <input v-model="state.search" type="text" />
+  <div @click="search">Submit</div>
 </template>
 
 <style scoped>
